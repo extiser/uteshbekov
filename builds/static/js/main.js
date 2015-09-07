4617c66cd88649450e921ba4843457ac4c754098
Руслан Усманов <extiser@MacBook-Pro-Ruslan.local>
2015-09-07 16:38:54 +0500

    mainpage

---
+++ builds/static/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('#slider').slick({
+        speed: 1300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        draggable: true,
+        fade: true,
+        dots: false
+    });
+});
